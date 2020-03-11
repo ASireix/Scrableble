243ba489bfe4a9c5fee13a7327fbc956586a75ae
Alexandre Sireix <asireix@pcalb-mm0000.univ-jfc.fr>
2020-03-11 11:33:04 +0100

    prions pour que ca marche

---
+++ src/scrable/Controleur.java
@@ -0,0 +1,10 @@
+package scrable;
+
+public class Controleur {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/scrable/Vue.java
@@ -0,0 +1,10 @@
+package scrable;
+
+public class Vue {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
